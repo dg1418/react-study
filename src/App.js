30e02faab4dc12ec8233d4adf6aefc95b8a50f47
dg1418 <dg1418@naver.com>
2025-07-08 10:20:08 +0900

    6장 Effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("count render");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("keyword render");
+  }, [keyword]);
+
   return (
-    <div className="App">
-      <h1>Welcome, back!</h1>
-      <Button text="Click me!" />
+    <div>
+      <h1>count: {count}</h1>
+      <h1>Keyword: {keyword}</h1>
+      <button onClick={onClick}>click</button>
+      <input type="text" onChange={onChange} />
     </div>
   );
 }
